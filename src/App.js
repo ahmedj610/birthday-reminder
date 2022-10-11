041a66afc76ee1bb26c6a95c8881620ad6579a2e
Software Guild <softwareguild2@gmail.com>
2022-10-11 15:30:18 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import List from './List';
 
 function App() {
 
-  function isHelping() {
-    true ?
-      console.log(true) :
-      console.log(false)
-  }
-
   const [people, setPeople] = useState(data)
   return (
     <main>
-      <section className="container" onClick={isHelping}>
+      <section className="container">
         <h3>{people.length} Birthdays Today</h3>
         <List
           people={people}
